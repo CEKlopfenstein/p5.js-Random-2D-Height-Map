ae34c4d343b12b5a29b28c67fcfeaf5df7711189
CEKlopfenstein <carlstrain1@gmail.com>
2020-01-23 08:58:41 -0500

    Set up basic Functionality Will Branch From Here

--- sketch.js
+++ sketch.js
@@ -1,9 +1,30 @@
 //Settings
-canvasSize = [640,480];
+var canvasSize = [640,480];
+var mapSize = [10,10];
+
+//Global variables
+var mapArray;
+
+//Objects
+function Cell(){
+  //Variables
+  this.x;
+  this.y;
+
+  //Initialization
+  
+
+  //Functions
+  this.hold = function() {
+    console.log("PlaceHolder");
+  }
+
+}
 
 function setup() {
   createCanvas(canvasSize[0],canvasSize[1]);
 }
+
 function draw() {
 
 }
