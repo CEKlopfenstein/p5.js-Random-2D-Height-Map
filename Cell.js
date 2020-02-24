b7b7e26bef8dcd210f0d1cc8b34d9e408f467577
CEKlopfenstein <carlstrain1@gmail.com>
2020-02-24 10:16:59 -0500

    Removed method in Cell that was missed before merge. Removed unneeded file.

--- Cell.js
+++ Cell.js
@@ -66,20 +66,6 @@ function Cell(xCord, yCord, xDem, yDem){
     }
   }
 
-  this.bottomOut = function(currentBlock){
-    for(var c = 0; c < this.neighbors.length; c++){
-      if(this.z - fallRate > 0){
-        if(this.neighbors[c].z == 0){
-          this.neighbors[c].z = this.z - fallRate;
-          divHighPoints[currentBlock].push([this.neighbors[c].x,this.neighbors[c].y])
-          finalprog++;
-        }
-      }else {
-        break;
-      }
-    }
-  }
-
   this.offset = function(xCord,yCord) {
     this.x = xCord;
     this.y = yCord;
