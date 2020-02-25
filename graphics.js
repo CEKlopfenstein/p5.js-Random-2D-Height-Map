b9c61838461eb6951ec461bdb12f88893b1f4d40
CEKlopfenstein <carlstrain1@gmail.com>
2020-02-25 10:15:31 -0500

    Removed code that was unneeded for this demo.

--- graphics.js
+++ graphics.js
@@ -18,62 +18,12 @@ function graphic(xOffset,yOffset) {
       if(xOffset > 0 && yOffset > 0){
         mapArray[x][y].offset(abs((x+xOffset)%mapArray.length),abs((y+yOffset)%mapArray[0].length));
       }
-      testColor2(mapArray[x][y]);
+      generic(mapArray[x][y]);
     }
   }
 }
 
-function testGraphic(xOffset,yOffset) {
-  for(var x = 0; x < mapArray.length; x++){
-    for(var y = 0; y < mapArray[0].length; y++){
-      if(xOffset > 0 && yOffset > 0){
-        mapArray[x][y].offset(abs((x+xOffset)%mapArray.length),abs((y+yOffset)%mapArray[0].length));
-      }
-      if(x < floor(mapArray.length/2)){
-        changeMapGrad(mapArray[x][y]);
-      }else{
-        testColor2(mapArray[x][y]);
-      }
-    }
-  }
-}
-
-//Basic coloring of hightmap that gives a look of terrain
 function generic(point) {
-  if(point.z <= 85){
-    point.show("#0056b8");
-  }else if(point.z <= 127){
-    point.show("#287ee0");
-  }else if(point.z <= 137){
-    point.show("#fad355");
-  }else if(point.z <= 204){
-    point.show("green");
-  }else{
-    mapArray[x][y].show("grey");
-  }
-}
-
-//Greyscale of the z cord
-function greyScale(point) {
-  point.show(255-point.z);
-}
-
-//The idea here is to make it a transition from blue to green
-function testColor1(point) {
-  point.show([10,point.z,255-point.z]);
-  /*
-  Conclusion While it works it does not have a good look. It becomes blobs of green in blue.
-  */
-}
-
-//Near clone of testColor1 just with lines
-function testColor1i1(point) {
-  point.grid([10,point.z,255-point.z]);
-}
-
-//This promising
-//This is what I am going to use.
-function testColor2(point) {
   if(point.z>(highestPoint-10)){
     for(c in point.neighbors){
       if(point.neighbors[c].z > (highestPoint-10)){
@@ -99,61 +49,5 @@ function testColor2(point) {
     point.show("#0056b8");
   }else if(point.z <= 127){
     point.show("#287ee0");
-  }
-  /*
-  This looks good but I want to see what it looks like when I change the sea level
-  */
-}
-
-//same as testColor2 but with a changing sea level
-function testColor2i2(point) {
-  var seaLevel = floor((highestPoint-lowestPoint)/2);
-  if(point.z>(highestPoint-10)){
-    for(c in point.neighbors){
-      if(point.neighbors[c].z > (highestPoint-10)){
-        point.show("grey");
-        break;
-      }
-    }
-  }else if(point.z > seaLevel){
-    var beach = false;
-    for(c in point.neighbors){
-      for(c1 in point.neighbors[c].neighbors){
-        if(point.neighbors[c].neighbors[c1].z <= seaLevel){
-          point.show("#fad355");
-          beach = true;
-          break;
-        }
-      }
-    }
-    if(!beach){
-      point.show("green");
-    }
-  }else if(point.z <= seaLevel-42){
-    point.show("#0056b8");
-  }else if(point.z <= seaLevel){
-    point.show("#287ee0");
-  }
-  /*
-  This looks good but I want to see what it looks like when I change the sea level
-  */
-}
-
-//Shows gradual change in z
-function changeMapGrad(point) {
-  point.findZChange();
-  var mostChange = fallRate;
-  point.show(255 - 255*(point.zChange/mostChange));
-}
-
-//Shows points when change in z reach an integer
-function changeMapPoint(point) {
-  point.findZChange();
-  var mostChange = fallRate/4;
-  for(var c = 1; c <= mostChange; c += 0.25){
-    if(point.zChange / 4 == c){
-      point.show(255 - 255*(c/mostChange));
-      break;
-    }
   }
 }
