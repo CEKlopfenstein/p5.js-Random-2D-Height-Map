252ad8d49deea317a7f011e32088b194507255f9
CEKlopfenstein <carlstrain1@gmail.com>
2020-03-18 10:32:34 -0400

    River Rewrite Working on making rivers their own objects. This is to help me more than anything else.

---
+++ river.js
@@ -0,0 +1,10 @@
+function River(startX, startY, xDem, yDem){
+  this.x = startX;
+  this.y = startY;
+  this.z = 0;
+  this.width = xDem;
+  this.height = yDem;
+  this.xTrue = startX*xDem;
+  this.yTrue = startY*yDem;
+  this.riverPath.
+}
